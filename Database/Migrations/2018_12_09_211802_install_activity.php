8dab68b8f4e10f451947afc7404d06791c64ad65
Unknown <blondin.boris@gmail.com>
2019-04-29 21:42:12 +0100

    renamed migration

--- Database/Migrations/2018_12_09_211802_install_activity.php
+++ Database/Migrations/2018_12_09_211802_install_activity.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Install extends Migration
+class InstallActivity extends Migration
 {
     /**
      * Run the migrations.
